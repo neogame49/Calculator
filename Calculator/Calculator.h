ba3f1c668d1bb2ddf71de2472ad5d6b04ab13e81
Macbook <Macbook@MacBook-Air-Macbook.local>
2015-02-20 14:23:32 +0200

    Initial Commit

---
+++ Calculator/Calculator.h
@@ -0,0 +1,19 @@
+//
+//  Calculator.h
+//  Calculator
+//
+//  Created by Macbook on 20.02.15.
+//  Copyright (c) 2015 Scherbiy Roman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Calculator.
+FOUNDATION_EXPORT double CalculatorVersionNumber;
+
+//! Project version string for Calculator.
+FOUNDATION_EXPORT const unsigned char CalculatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Calculator/PublicHeader.h>
+
+
